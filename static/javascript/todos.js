eda7ab0a60ecea6c1044332d1063d66df9b97b95
Zhen Jun Xu <arlenxuzj@gmail.com>
2022-11-19 22:05:11 -0500

    refactor: remove unused code

--- static/javascript/todos.js
+++ static/javascript/todos.js
@@ -26,25 +26,4 @@ const toggleCompleted = async todo => {
   } catch (error) {
     console.error(error.message);
   }
-
-  // fetch(`/todos/${id}`, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json'
-  //   },
-  //   body: JSON.stringify({ id })
-  // })
-  //   .then(response => {
-  //     const data = response.json();
-  //     if (response.status === 200) {
-  //       console.log(`Updated todo: ${JSON.stringify(data)}`);
-  //       // Reload the page
-  //       window.location.reload();
-  //     } else {
-  //       throw new Error(data);
-  //     }
-  //   })
-  //   .catch(error => {
-  //     console.error(error.message);
-  //   });
 };
